d78f9f6513d279a83707c2fc2308c5ccfa8ed4df
Juan C. Luna H. <jcluna.oficial@gmail.com>
2017-02-22 22:24:02 -0600

    UDbParametro información para un parámetro

--- UDbParameter.cs
+++ UDbParameter.cs
@@ -1,9 +1,58 @@
-﻿namespace JclunaOficial
+﻿using System;
+using System.Data;
+
+namespace JclunaOficial
 {
     /// <summary>
     /// Parámetro de datos
     /// </summary>
-    public static class UDbParameter
+    public class UDbParameter
     {
+        /// <summary>
+        /// Nombre del parámetro
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Tipo de dato del parámetro
+        /// </summary>
+        public DbType Type { get; set; }
+
+        /// <summary>
+        /// Valor del parámetro
+        /// </summary>
+        public object Value { get; set; }
+
+        /// <summary>
+        /// Dirección del parámetro (input | output)
+        /// </summary>
+        public ParameterDirection Direction { get; set; }
+
+        /// <summary>
+        /// Crear una instancia del tipo <see cref="UDbParameter"/>
+        /// </summary>
+        public UDbParameter()
+        {
+            // inicializar atributos
+            Name = string.Empty;
+            Type = DbType.String;
+            Value = DBNull.Value;
+            Direction = ParameterDirection.Input;
+        }
+
+        /// <summary>
+        /// Crear una instancia del tipo <see cref="UDbParameter"/>
+        /// </summary>
+        /// <param name="name">Nombre del parámetro</param>
+        /// <param name="type">Tipo de datos del parámetro</param>
+        /// <param name="value">Valor del parámetro</param>
+        /// <param name="direction">Dirección del parámetro</param>
+        public UDbParameter(string name, DbType type, object value, ParameterDirection direction = ParameterDirection.Input)
+        {
+            Name = name;
+            Type = type;
+            Value = (value != null ? value : DBNull.Value);
+            Direction = direction;
+        }
     }
 }
